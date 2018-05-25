50b6e67c986ca7f069099b4ec84981048bbded72
JadeDoo <610518278@qq.com>
2018-05-26 00:52:39 +0800

    完成header组件

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,71 @@
 <template>
   <div id="app">
-    <!-- <img src="./assets/logo.png"> -->
-    <router-view/>
+    <v-header :seller="seller"></v-header>
+    <div class="tab border-1px">
+      <div class="tab-item">
+        <router-link to="/goods" exact>商品</router-link>
+        </div>
+      <div class="tab-item">
+        <router-link to="/ratings">评论</router-link>
+      </div>
+      <div class="tab-item">
+        <router-link to="/seller">商家</router-link>
+      </div>
+    </div>
+    
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
+import header from "@/components/header/header";
 export default {
-  name: 'App'
-}
+  data() {
+    return {
+      seller: {}
+    };
+  },
+  created() {
+    this.$http.get("/api/seller").then(response => {
+      response = response.body;
+      if (response.errno === 0) {
+        this.seller = response.data;
+      }
+    });
+  },
+  components: {
+    "v-header": header
+  }
+};
 </script>
 
-<style>
+<style lang='scss'>
+@import './common/scss/mixin.scss';
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  .tab {
+    display: flex;
+    width: 100%;
+    height: 40px;
+    line-height: 40px;
+    // border-bottom: 1px solid rgba(7,15,27,0.1);
+    @include border-1px(rgba(7, 15, 27, 0.1));
+
+    .tab-item {
+      flex: 1;
+      text-align: center;
+
+      & > a {
+        display: block;
+        font-size: 14px;
+        color: rgb(77, 85, 93);
+
+        &.router-link-active {
+          color: red;
+          box-shadow: 0 0 5px #000 inset;
+        }
+      }
+    }
+  }
 }
 </style>
---
+++ src/components/goods/goods.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="header">我是商品页面</div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="stylus">
+
+</style>
